86b37f2826e07fe421e901ba4f1dafff270d3f94
Salvatore Misiano <104531745+SalvatoreMisia@users.noreply.github.com>
2025-04-11 01:51:01 +0200

    refactor

--- vst3_list.py
+++ vst3_list.py
@@ -8,86 +8,112 @@ Created on Sun Oct 22 13:19:47 2023
 ####~ Modules recall ~#########################################################
 import pandas as pd
 import os
-from os.path import isdir, join, basename
+from os.path import join, basename, splitext, commonpath, relpath
+from pathlib import PurePath
+
+####~ Constants ~##############################################################
+VST3_EXTENSIONS = ['.vst3', '.dll']
+EXCLUDED_EXTENSIONS = ['.ini']
+NOT_RECOGNIZED_LABEL = '_Not recognized'
+UNKNOWN_LABEL = '_Unknown'
 
 ####~ Functions ~##############################################################
 def filepathinfo(curr_path, base_path):
+    """
+    Extracts file information such as basename, extension, common prefix, and relative path.
+    """
     file_basenm = basename(curr_path)
-    file_extens = os.path.splitext(curr_path)[-1].lower()
-    path_prefix = os.path.commonpath([curr_path, base_path])
-    path_suffix = os.path.relpath(os.path.dirname(curr_path), path_prefix)
+    file_extens = splitext(curr_path)[-1].lower()
+    path_prefix = commonpath([curr_path, base_path])
+    path_suffix = relpath(os.path.dirname(curr_path), path_prefix)
     return file_basenm, file_extens, path_prefix, path_suffix
 
-def pthdirnav(path_start):
-    vst3_extns = ['.vst3', '.dll']
-    excl_extns = ['.ini'] # excluded files
-    
-    list_pth_el = os.listdir(path_start)
-    full_paths = [join(path_start, i) for i in list_pth_el]
-    list_sub_pths, list_files = list(), list()
-    for curr_path in full_paths:
-        _, file_ext, _, _ = filepathinfo(curr_path, path_start)
-        
-        if isdir(curr_path) and not(any([x == file_ext for x in vst3_extns])): # It works also with vst2
-            list_sub_pths.append(curr_path)
-            
-        elif any([x == file_ext for x in excl_extns]):
-            continue # Nothing to do, just excluded hidden files
-            
-        else:
-            list_files.append(curr_path)
-            
-    return list_sub_pths, list_files
+def is_subpath(parent_path, child_path):
+    """
+    Detect if a certain path is a subpath of another.
+    """
+    parent = PurePath(parent_path)
+    child = PurePath(child_path)
+    try:
+        child.relative_to(parent)
+        return True
+    except ValueError:
+        return False
+
+def list_files_and_dirs(path_start):
+    """
+    Lists all files and directories in the given path, excluding certain extensions.
+    """
+    list_sub_pths, list_files = [], []
+    for root, dirs, files in os.walk(path_start):
+        for dir in dirs:
+            path_dir = join(root, dir)
+            _, file_ext, _, _ = filepathinfo(path_dir, path_start)
+            if file_ext in VST3_EXTENSIONS:
+                list_files.append(path_dir)
+            else:
+                list_sub_pths.append(path_dir)
+        for file in files:
+            path_file = join(root, file)
+            _, file_ext, _, _ = filepathinfo(path_file, path_start)
+            if file_ext in EXCLUDED_EXTENSIONS:
+                continue
+            else: # only add files that are not inside .vst3 folder
+                # if not any([y in [commonpath([path_file, x]) for x in list_files] for y in list_files]):
+                if not any([is_subpath(x, path_file) for x in list_files]):
+                    list_files.append(path_file)
+    return list_files, list_sub_pths
 
 def nameofvst(curr_path, start_path):
-    vst3_extns = ['.vst3', '.dll']
-    filename_vst = str()
-    
-    flnm_crr, file_ext, _, pth_suff = filepathinfo(curr_path, start_path)
-    filename_gen = join(pth_suff, flnm_crr)
-    if any([x == file_ext for x in vst3_extns]): # It works also with vst2
-        filename_vst = flnm_crr
-    return filename_vst, filename_gen
+    """
+    Determines the name of the VST plugin and its relative path.
+    """
+    filename_vst, file_ext, _, pth_suff = filepathinfo(curr_path, start_path)
+    if pth_suff == '.':
+        manufacturer = UNKNOWN_LABEL
+    else:
+        manufacturer = pth_suff
+    filename_gen = join(pth_suff, filename_vst)
+    if file_ext in VST3_EXTENSIONS:
+        return filename_vst, filename_gen, manufacturer
+    return "", filename_gen, NOT_RECOGNIZED_LABEL
+
+def process_vst3_folder(path_vst3):
+    """
+    Processes the VST3 folder and organizes plugins by manufacturer.
+    """
+    all_files, _ = list_files_and_dirs(path_vst3)
+
+    vst_dict = {UNKNOWN_LABEL: [], NOT_RECOGNIZED_LABEL: []}
+    for curr_path in all_files:
+        curr_vst_file, curr_gen_file, curr_vst_dev = nameofvst(curr_path, path_vst3)
+        if curr_vst_dev not in list(vst_dict.keys()):
+            vst_dict[curr_vst_dev] = []
+        if curr_vst_file:
+            vst_dict[curr_vst_dev].append(curr_vst_file)
+        else:
+            vst_dict[curr_vst_dev].append(curr_gen_file)
+
+    return vst_dict
+
+def save_to_csv(vst3_data, path_rprt):
+    """
+    Saves the VST3 data to CSV files.
+    """
+    path_sprd = join(path_rprt, 'VST3_List.csv')
+    path_chck = join(path_rprt, 'VST3_2Check.csv')
+
+    vst3_df = pd.DataFrame.from_dict(vst3_data, orient='index').transpose()
+    vst3_df.to_csv(path_or_buf=path_sprd, columns=[UNKNOWN_LABEL, NOT_RECOGNIZED_LABEL], index=False)
+    vst3_df.to_csv(path_or_buf=path_chck, columns=[col for col in vst3_df.columns if col not in [UNKNOWN_LABEL, NOT_RECOGNIZED_LABEL]], index=False)
 
 ####~ Core ~###################################################################
-nt_rc_lbl = '_Not recognized'
-uknwn_lbl = '_Unknown'
+if __name__ == "__main__":
+    """
+    If you run this module as a script, then the following lines will be executed
+    """
+    path_vst3 = input('VST3 folder ([C:\\Program Files\\Common Files\\VST3]): ') or 'C:\\Program Files\\Common Files\\VST3'
+    path_rprt = input(f'Excel folder ([{os.getcwd()}]): ') or os.getcwd()
 
-path_vst3 = input('VST3 folder ([C:\\Program Files\\Common Files\\VST3]): ') or 'C:\\Program Files\\Common Files\\VST3'
-path_rprt = input(f'Excel folder ([{os.getcwd()}]): ') or os.getcwd()
-
-path_sprd = join(path_rprt, 'VST3_List.csv')
-path_chck = join(path_rprt, 'VST3_2Check.csv')
-
-temp_pths, nodir_vst = pthdirnav(path_vst3)
-
-vst_flds_raw = {uknwn_lbl: nodir_vst}
-strt_fld_raw = {uknwn_lbl: path_vst3}
-for curr_path in temp_pths:
-    strt_fld_raw[basename(curr_path)] = curr_path
-    vst_flds_raw[basename(curr_path)] = list()
-    
-    temp_sub_flds = [curr_path]
-    while len(temp_sub_flds) >= 1:
-        tmp_subs, tmp_files = pthdirnav(temp_sub_flds[0])
-        vst_flds_raw[basename(curr_path)] += tmp_files
-        temp_sub_flds += tmp_subs
-        temp_sub_flds.pop(0)
-
-list_vst3 = {nt_rc_lbl: list()} # Dictionary type, initializing
-for x in vst_flds_raw:
-    list_vst3[x] = list()
-    
-for curr_dev, curr_files in vst_flds_raw.items():
-    for curr_pth in curr_files:
-        # temp_start_pth = strt_fld_raw[curr_dev]
-        curr_vst, curr_gen = nameofvst(curr_pth, path_vst3)
-        if not(len(curr_vst) == 0):
-            list_vst3[curr_dev] += [curr_vst]
-        else:
-            list_vst3[nt_rc_lbl] += [curr_gen]
-
-vst3_df = pd.DataFrame.from_dict(list_vst3, orient='index').transpose()
-
-vst3_df.to_csv(path_or_buf=path_sprd, columns=vst3_df.columns[2:], index=False)
-vst3_df.to_csv(path_or_buf=path_chck, columns=vst3_df.columns[:2], index=False)
+    vst3_data = process_vst3_folder(path_vst3)
+    save_to_csv(vst3_data, path_rprt)
